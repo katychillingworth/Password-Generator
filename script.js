8077e7d8c88bc8872d5ee4af1038107adf9e33bd
katychillingworth <katy.chillingworth@hotmail.com>
2023-01-23 20:04:24 +0000

    adds math.random code

--- script.js
+++ script.js
@@ -88,54 +88,66 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Add event listener to bring up the first prompt
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var item = arr[randomIndex];
+  return item;
+}
+
+// Add event listener to bring up the first prompt:
 var generateButton = document.querySelector("#generate");
 
 generateButton.addEventListener("click", function () {
   getPasswordOptions()
 })
 
-// Function to prompt user for password options
+// Function to prompt user for password options:
 function getPasswordOptions() {
-  numericCharacters = prompt("Please choose a password length between 10 and 64 characters:")
-if (numericCharacters < 10) {
-  alert ("Sorry! Please select a number higher than 10");
-  getPasswordOptions()
-}
-else if (numericCharacters > 64) {
-  alert ("Sorry! Please select a number lower than 64");
-  getPasswordOptions()
-}
-else {
-  characterChoices()}
-  function characterChoices() {
-    upperCasedCharacters = confirm ("Would you like your password to contain uppercase characters?");
-    lowerCasedCharacters = confirm ("Would you like your password to contain lowercase characters?");
-    numericCharacters = confirm }
-}
+  var passwordLength = prompt("Please choose a password length between 10 and 64 characters:")
+  if (passwordLength < 10) {
+    alert("Sorry! Please select a number higher than 10");
+    getPasswordOptions()
+  }
+  else if (passwordLength > 64) {
+    alert("Sorry! Please select a number lower than 64");
+    getPasswordOptions()
+  }
+  else {
+    var allowUpperCasedCharacters = confirm("Would you like your password to contain uppercase characters?");
+    var allowLowerCasedCharacters = confirm("Would you like your password to contain lowercase characters?");
+    var allowNumericCharacters = confirm("Would you like your password to contain numerical characters?");
+    var allowSpecialCharacters = confirm("Would you like your password to contain special characters?");
 
-// Function for getting a random element from an array
-function getRandom(arr) {
-
+    // TODO: Generate password using choices
+  }
 }
 
 // Function to generate password with user input
+// TODO: Add parameters for all 5 options
 function generatePassword() {
+  // TODO: Define password generation algorithm
+  // Important factors are:
+  // - Password length
+  // - Randomised character from _all_ potential options
 
+  // var password = "";
+  // for (var i = 0; i < numericCharacters; i++) {
+  //   password = password + getRandom(characterChoices);
+  //   return password
+  // }
 }
 
-// Done
-
-// Done Get references to the #generate element
+// Get references to the #generate element
+generatePassword()
 var generateBtn = document.querySelector('#generate');
 
-// Done Write password to the #password input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
 
-// Done Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
